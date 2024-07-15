c828c39a4aa012966cf423cf9ead9ffd8ceac64b
Nicvolpe <130798898+Nicvolpe@users.noreply.github.com>
2024-07-15 10:34:04 -0300

    probar usuarios.js

--- js/usuarios.js
+++ js/usuarios.js
@@ -9,8 +9,8 @@ createApp({
             cargando: true,
             /*alta*/
             id: 0,
-            usuario: "admin",
-            clave: "admin",
+            usuario: "Introduzca un usuario",
+            clave: "Introduzca una contraseña",
             rol:1,
         }
     },
@@ -29,8 +29,7 @@ createApp({
                     this.error = true
                 })
         },
-        /*
-        eliminar(id) {
+    eliminar(id) {
             const url = 'https://NicoV.pythonanywhere.com/usuarios/' + id;
             var options = {
                 method: 'DELETE',
@@ -63,11 +62,11 @@ createApp({
                     alert("Error al Grabarr")
                 })
         },
-        /*  modificar() {
+        modificar() {
               let usuario = {
                   usuario:this.usuario,
                   clave: this.clave,
-                  
+                  rol: this.rol,
               }
               var options = {
                   body: JSON.stringify(usuario),
@@ -85,6 +84,7 @@ createApp({
                       alert("Error al Modificar")
                   })      
           },*/
+        
         login() {
             usuario=this.usuario
             sessionStorage.setItem("adm",0)
@@ -95,21 +95,22 @@ createApp({
             if (i<(this.usuarios.length)){
                 if (this.usuarios[i].clave==this.clave ){
                     if (this.usuarios[i].rol==1){
-                        sessionStorage.setItem("adm",1)  
-                        
+                        sessionStorage.setItem("adm",1)            
                     }
                     window.location.href = "./index.html";
                 }else{
             /*        if (this.usuarios[i].rol==0){
                         sessionStorage.setItem("adm",0)
-                        
                     }
+                    
+                    window.location.href = "./index.html";
             */
                     alert('Clave erronea')
                 }
             }else{
                 alert('Usuario erronea')
             }
+            
             /*for (elemento of this.usuarios){
                 if (elemento.usuario == this.usuario && elemento.clave==this.clave ){
                     if (elemento.rol=1){
