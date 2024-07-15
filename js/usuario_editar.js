1cd0dd9f0823e00f18c33320883a3a880ee53394
Nicvolpe <130798898+Nicvolpe@users.noreply.github.com>
2024-07-15 10:44:32 -0300

    usuario_editar.js

---
+++ js/usuario_editar.js
@@ -0,0 +1,58 @@
+console.log(location.search)     // lee los argumentos pasados a este formulario
+var id=location.search.substr(4)  // usuario_update.html?id=1
+console.log(id)
+const { createApp } = Vue
+  createApp({
+    data() {
+      return {
+        id:0,
+        nombre:"",
+        clave:"",
+        rol:0,
+       url:'http://NicoV.pythonanywhere.com/usuarios/'+id,
+       // url:'http://127.0.0.1:5000/productos/'+id,
+       }  
+    },
+    methods: {
+        fetchData(url) {
+            fetch(url)
+                .then(response => response.json())
+                .then(data => {
+                    console.log(data)
+                    this.id=data.id
+                    this.nombre = data.nombre;
+                    this.clave=data.clave
+                    this.rol=data.rol
+                })
+                .catch(err => {
+                    console.error(err);
+                    this.error=true              
+                })
+        },
+        modificar() {
+            let usuario = {
+                nombre:this.nombre,
+                clave: this.clave,
+                rol: this.rol,
+            }
+            var options = {
+                body: JSON.stringify(usuario),
+                method: 'PUT',
+                headers: { 'Content-Type': 'application/json' },
+                redirect: 'follow'
+            }
+            fetch(this.url, options)
+                .then(function () {
+                    alert("Registro modificado")
+                    window.location.href = "./usuarios.html"; // navega a usuarios.html          
+                })
+                .catch(err => {
+                    console.error(err);
+                    alert("Error al Modificar")
+                })      
+        }
+    },
+    created() {
+        this.fetchData(this.url)
+    },
+  }).mount('#app')
